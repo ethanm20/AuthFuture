a42733cfe1e80f0a04ca78c7b0d0d12db88dd341
Ethan Marlow <z5312704@ad.unsw.edu.au>
2025-03-08 22:53:10 +1100

    Update

--- auth-future/src/pages/TOTP-Tool/TOTP-Tool.js
+++ auth-future/src/pages/TOTP-Tool/TOTP-Tool.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 export function TOTPTool() {
     return (
         <>
-                <h2>Time-Based One Time Password (TOTP) Tool</h2>
+        <h2>Time-Based One Time Password (TOTP) Tool</h2>
         
         <div id="totp-tool-container">
             <div id="generating-qr-code">
@@ -12,13 +12,22 @@ export function TOTPTool() {
                 <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
 
                 <div id="otp-secret">
-                    <div id="otp-qr-code"></div>
-                    
-                    <b>Secret Key: </b><span id="otp-secret-code"></span><br/>
-                    <b>QR Code Content: </b><span id="otp-qr-code-text"></span>
-                    <input type="text"></input>
-                    <Button variant="primary">Randomly Generate</Button>
+                    <div id="qr-code">
+                        <div id="otp-qr-code"></div>
+                        <b>QR Code Content: </b><span id="otp-qr-code-text"></span>
+                    </div>
+                    <div id="key-section">
+                        <b>Secret Key (Base 32): </b><span id="otp-secret-code"><input type="text"></input></span><br/>
+                        <Button variant="secondary">Randomly Generate</Button>
+
+                        <Button variant="primary">Submit</Button>
+                    </div>
                 </div>
+            </div>
+            <div id="validate-totp">
+                <h3>Step 2: Validate 6 Digit Code</h3>
+
+
             </div>
         </div>
         </>
