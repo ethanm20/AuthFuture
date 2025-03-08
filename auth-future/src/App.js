a110883cb79a254ed038e88665db20afbd9b57da
Ethan Marlow <z5312704@ad.unsw.edu.au>
2025-03-08 17:39:58 +1100

    Update

--- auth-future/src/App.js
+++ auth-future/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<NavigationBar/>}>
               <Route index element={<Home/>} />
